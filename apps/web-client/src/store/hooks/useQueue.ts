5263823fd70918de8294ff48357f6e9728842394
whimsypingu <blindpingu@gmail.com>
2026-04-05 11:35:40 -0700

    bugfix for optimistic queue state

--- apps/web-client/src/store/hooks/useQueue.ts
+++ apps/web-client/src/store/hooks/useQueue.ts
@@ -29,21 +29,30 @@ export const useQueue = () => {
             const response = await fetch(`/queue/set-first?track_id=${track.id}`, { method: "POST" });
 
             if (!response.ok) throw new Error("Failed to set first entry in queue");
-            return await response.json();
+
+            const data = await response.json();
+            console.log(data);
+            return data;
+            // return await response.json();
         },
         onMutate: async (track: TrackBase) => {
             //audio engine immediately here?
-            await audio.playTrack(track.id); //maybe swap to fire and forget?
+            audio.playTrack(track.id).catch(e => console.error("Audio failed:", e));
             
             await queryClient.cancelQueries({ queryKey }); // cancel outgoing refetches so they dont rewrite optimistic changes
 
             const rollbackQueue = queryClient.getQueryData(queryKey); //get the rollback state
 
             const queueTrack = toQueueTrackWithQueueId(track, -1); //typecast to a QueueTrack with -1 default queueId field
+            console.log("queueTrack:", queueTrack);
 
-            queryClient.setQueryData(queryKey, (old: QueueTrack[]) => {
-                if (!old) return [queueTrack];
-                return [queueTrack, old.slice(1)];
+            queryClient.setQueryData(queryKey, (old: QueueTrack[] | undefined) => {
+                const result = [queueTrack, ...(old?.slice(1) || [])];
+
+                console.log("optimistic state:", result);
+
+                return result;
+                // return [queueTrack, old.slice(1)];
             })
 
             return { rollbackQueue }; //return context for rollback
@@ -55,6 +64,10 @@ export const useQueue = () => {
             console.log("Optimistic setFirst failed, rolling back.");
         },
         onSuccess: (data) => {
+            console.log("Mutation Success. Full Data:", data);
+            if (!data.queue) {
+                console.error("CRITICAL: 'queue' field missing in response! Check FastAPI return shape.");
+            }
             queryClient.setQueryData(queryKey, data.queue); //immediately swap the optimistic -1 queueId for DB-assigned queueId
         }
     });
