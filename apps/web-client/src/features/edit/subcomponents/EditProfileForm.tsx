36df17b7c2285b93a77c177e4fa9a99a9e7cfe58
whimsypingu <blindpingu@gmail.com>
2026-06-05 11:50:11 -0700

    ui adjusted to use input instead of textarea for username

--- apps/web-client/src/features/edit/subcomponents/EditProfileForm.tsx
+++ apps/web-client/src/features/edit/subcomponents/EditProfileForm.tsx
@@ -6,6 +6,7 @@ import { HoldToDeleteButton } from "@/components/ui/hold-delete";
 
 import { MIN_BUTTON_WIDTH, SOURCE_ICON_SIZE } from "@/features/edit/edit.constants";
 import type { UserStats } from "@/features/profile/profile.types";
+import { Input } from "@/components/ui/input";
 
 
 interface EditProfileFormProps {
@@ -27,15 +28,15 @@ export const EditProfileForm = ({
         <div className="flex flex-col h-full">
             <div className="h-full custom-scrollbar overflow-y-auto flex flex-col gap-4">
                 {/* Username Section */}
-                <div className="flex flex-col gap-1">
+                <div className="flex flex-row gap-3 items-center">
                     <label className="text-sm font-medium text-muted-foreground">
                         Username
                     </label>
-                    <Textarea
-                        value={stats.username}
+                    <Input
+                        value={usernameInput}
                         onChange={(e) => setUsernameInput(e.target.value)}
                         placeholder={stats.username}
-                        className="text-md focus-visible:ring-1"
+                        className="text-md"
                     />
                 </div>
             </div>
