cb50513fc59b1b6be35e42ed2d7bdad134499091
whimsypingu <blindpingu@gmail.com>
2026-08-12 17:44:15 -0700

    initial logic for websocket handling within the RoomManager

--- apps/audio-server/core/room/room_manager.py
+++ apps/audio-server/core/room/room_manager.py
@@ -1,8 +1,10 @@
 import asyncio
 import logging
+from typing import Any
 
 from core.models.room import Device, DeviceContext, Room
 from database.database_manager import DatabaseManager
+from fastapi import WebSocket
 
 logger = logging.getLogger(__name__)
 
@@ -30,7 +32,7 @@ class RoomManager:
         self.is_running = True
 
 
-    def device_active(self, room_id: str, device_id: str):
+    def device_active(self, room_id: str, device_id: str) -> Device:
         """Given a room_id and device_id 'touch' the device to note it is active"""
         #change room mapping if necessary
         last_room_id = self.device_to_room[device_id]
@@ -49,9 +51,58 @@ class RoomManager:
             self.rooms[room_id] = Room(id=room_id)
 
         #add/touch the device
-        self.rooms[room_id].add_or_touch_device(device_id)
+        device = self.rooms[room_id].add_or_touch_device(device_id)
         self.device_to_room[device_id] = room_id #update lookup table
-        
+
+        return device
+
+
+    def connect_websocket(self, room_id: str, device_id: str, websocket: WebSocket) -> Device:
+        """Attaches WebSocket"""
+        device = self.device_active(room_id, device_id)
+        device.websocket = websocket
+
+        logger.info(f"Device '{device_id}' bound WebSocket in room '{room_id}'.")
+
+        return device
+
+    def disconnect_websocket(self, room_id: str, device_id: str):
+        """Detaches WebSocket"""
+        room = self.rooms.get(room_id)
+        if room and device_id in room.devices:
+            device = room.devices[device_id]
+            device.websocket = None
+
+            logger.info(f"Device '{device_id}' detached WebSocket in room '{room_id}'.")
+
+    async def broadcast_room(self, room_id: str, message: dict[str, Any]) -> None:
+        """Sends a JSON message to active WebSockets in a room"""
+        room = self.rooms.get(room_id)
+        if not room:
+            return
+
+        failed_device_ids: list[str] = []
+        for device_id, device in room.devices.items():
+            #skip over nonexistent websockets
+            if not device.websocket:
+                continue
+
+            try:
+                await device.websocket.send_json(message)
+            except Exception:
+                failed_device_ids.append(device_id)
+
+        #disconnect failed connections
+        for failed_device_id in failed_device_ids:
+            self.disconnect_websocket(room_id, failed_device_id)
+
+
+    async def broadcast_all(self, message: dict[str, Any]) -> None:
+        """Sends a JSON message to every active WebSocket across all rooms"""
+        for room_id in self.room.keys():
+            await self.broadcast_room(room_id, message)
+
+
 
     async def update_last_active(self, room_id: str, last_active: int):
         """Updates the listened at buffer with the most recent value"""
