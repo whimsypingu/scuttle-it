4e9945948144d1fc16ee7e8b6249cde01fd03669
whimsypingu <blindpingu@gmail.com>
2026-08-25 11:02:01 -0700

    some added persistence across reloads when offline. works nieely for endpoints that have already been seen (for example, when opening the library tab and then opening playlist 1, the legible contents of playlist 1 are at least stored and recoverable in an offline setting. however, playlist 2 which was not opened, will load as blank). will probably keep this addition, and add features for seamless offline playback wherever possible within tanstack

--- apps/web-client/src/App.tsx
+++ apps/web-client/src/App.tsx
@@ -1,6 +1,9 @@
 import { lazy, Suspense, useEffect, useState } from 'react'
 
-import { QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClientProvider } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { queryClient, persister } from '@/store/queryClient';
+
 import { MainLayout } from '@/features/player/MainLayout';
 
 import { OfflineProvider } from '@/features/offline/OfflineProvider';
@@ -10,7 +13,6 @@ import { NavBar } from '@/features/player/NavBar';
 import { Toast } from '@/features/toast/Toast';
 
 import type { Tab } from '@/features/player/player.types';
-import { queryClient } from '@/store/queryClient';
 import { AudioLogic } from '@/features/audio/AudioLogic';
 import { SyncLogic } from '@/store/sync/SyncLogic';
 
@@ -100,7 +102,15 @@ function App() {
 
 
 	return (
-		<QueryClientProvider client={queryClient}>
+		// <QueryClientProvider client={queryClient}>
+		<PersistQueryClientProvider
+			client={queryClient}
+			persistOptions={{
+				persister,
+				maxAge: 1000 * 60 * 60 * 24 * 7, //7 days cache validity
+				buster: "persister-v1", //dev
+			}}
+		>
 		<OfflineProvider>
 		<AuthGate>
 			<AudioLogic />
@@ -134,7 +144,8 @@ function App() {
 			</EditProvider>
 		</AuthGate>
 		</OfflineProvider>
-		</QueryClientProvider>
+		</PersistQueryClientProvider>
+		// </QueryClientProvider>
 	);
 }
 
--- apps/web-client/src/store/queryClient.ts
+++ apps/web-client/src/store/queryClient.ts
@@ -1,10 +1,21 @@
+import { get, set, del } from 'idb-keyval';
 import { QueryClient } from '@tanstack/react-query';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
 
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             staleTime: 1000 * 60 * 5, //5 minutes staleness
             refetchOnWindowFocus: true,
+            gcTime: 1000 * 60 * 60 * 24, //24 hrs: https://tanstack.com/query/latest/docs/framework/react/plugins/persistQueryClient
         }
     }
-})
+});
+
+export const persister = createAsyncStoragePersister({
+    storage: {
+        getItem: (key) => get(key),
+        setItem: (key, value) => set(key, value),
+        removeItem: (key) => del(key),
+    },
+});
